3e8071308d636c7a7882095b78b0f0b44c5b1261
lwch <lwch748@gmail.com>
2015-04-16 17:59:57 +0800

    修正logout时没有刷新页面导致部分页面显示异常的BUG

--- framework/footer.php
+++ framework/footer.php
@@ -7,8 +7,7 @@
 $(function() {
     $('#__logout__').click(function() {
         call('/api/logout.php', {}, function() {
-            $('#user-menu').empty();
-            $('#user-menu').append($('<li><a href="register.php">Register</a></li><li><a href="login.php">Login</a></li>'));
+            location.reload();
         });
     });
 })
